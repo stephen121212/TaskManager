e4c3999110e6f20f4fc499a2738c0098b9d92c10
stephen121212 <minechart1@gmail.com>
2020-04-17 17:13:21 +0100

    Adding Asynchronous Methods

--- src/index.js
+++ src/index.js
@@ -8,25 +8,73 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/User', (req, res) => {
+app.post('/User', async (req, res) => {
 
     const user = new User(req.body)
 
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
+    try{
+        await user.save()
+        res.status(201).send(user)
+    } catch(e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.post('/Task', (req, res) => {
+app.get('/User', async (req, res) => {
+    try{
+        const users = await User.find({})
+        res.send(users)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+app.get('/User/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try{
+        const user = await User.findById(_id)
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+app.post('/Task', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
-        res.send(task)
-    }).catch((e) => {
+    try{
+        await task.save()
+        res.status(201).send(user)
+    } catch(e) {
         res.status(400).send(e)
-    })
+    }
+})
+
+app.get('/Task', async (req, res) => {
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+app.get('/Task/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try{
+        const task = await Task.findById(_id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e) {
+        res.status(500).send()
+    }
 })
 
 app.listen(port, () => {
