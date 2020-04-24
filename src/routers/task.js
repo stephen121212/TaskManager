a187b8dfed6d763bbdccaa1b0fbc4998e92bf17d
stephen121212 <minechart1@gmail.com>
2020-04-24 13:03:35 +0100

    a

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const Task = require('../models/Task')
+const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
